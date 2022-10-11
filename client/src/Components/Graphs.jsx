7da2ec9162037fe1d56fdbb568a77f3c573656cd
cpa4001 <cpapostoli4001@eagle.fgcu.edu>
2022-10-10 21:56:57 -0400

    fixed dates on x axis, y axis scaling, top coin date, replaced fake chart with linechart of graphdata

--- client/src/Components/Graphs.jsx
+++ client/src/Components/Graphs.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState} from 'react'
 import { Chart } from "react-google-charts";
 
 export const fake = [
-  ["TIMESTAMP", "COIN_NAME"],
+  ["TIMESTAMP", "PRICE"],
   ["Mon", 20],
   ["Tue", 31],
   ["Wed", 50],
@@ -14,7 +14,7 @@ export const fake = [
 export const options = {
   title: "Coin Name",
   hAxis: { title: "24h", titleTextStyle: { color: "#333" } },
-  vAxis: { minValue: 0 },
+  //vAxis: { minValue: 0 },
   chartArea: { width: "70%", height: "70%" },
   legend: "none",
 };
@@ -41,7 +41,7 @@ function Graphs() {
     function convertDate(date) {
       var std = new Date(date * 1000); // convert to UTC from Epoch
       var shortDate = [
-        std.getMonth(),
+        std.getMonth() + 1, // JS months start at 0
         std.getDate(),
         std.getFullYear(),
       ].join('/');
@@ -57,10 +57,14 @@ function Graphs() {
       return time;
     }
 
+    function convertEpoch(epoch) {
+      return new Date(epoch * 1000)
+    }
+
     const makeGraph = () => {
       for (let x = 0; x < data.coin_data.history.length; x++) {
         
-        graphData.push([convertTime(data.coin_data.history[x].timestamp), parseFloat(data.coin_data.history[x].price)]); 
+        graphData.push([convertEpoch(data.coin_data.history[x].timestamp), parseFloat(data.coin_data.history[x].price)]); 
         console.log(graphData[x]);
       }
       //console.log(graphRows[0])
@@ -75,7 +79,8 @@ function Graphs() {
                   {
                     makeGraph()
                   }
-                <h1 className='title'>COIN_NAME > [${data.coin_data.change}] change > {convertDate(data.coin_data.history[1].timestamp)}</h1>
+                  <h1 className='title'>COIN_NAME  [${data.coin_data.change}] change &nbsp;
+                  {convertDate(data.coin_data.history[0].timestamp)}</h1>
                 <Chart
                   chartType="AreaChart"
                   width="100%"
@@ -84,10 +89,10 @@ function Graphs() {
                   options={options}
                 />
                 <Chart
-                  chartType="AreaChart"
+                  chartType="LineChart"
                   width="100%"
                   height="400px"
-                  data={fake}
+                  data={graphData}
                   options={options}
                 />
                 </div>
