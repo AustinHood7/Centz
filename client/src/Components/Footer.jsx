6119ba59cbb3fab60c2bf739ce0fa7db603d28e2
cpa4001 <cpapostoli4001@eagle.fgcu.edu>
2022-11-21 11:20:17 -0500

    ran build for POST graphs

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,13 +1,17 @@
 function Footer() {
-  
-    return (
-        <div className="bottomFooter">
-            <footer>
-                <p>Created by C. Apostoli, J. Hanson & A. Hood - 2022</p>
-                <p>Data provided by <a href="https://coinranking.com/" target="_blank">Coinranking</a></p>
-            </footer>
-        </div>
-    );
-  }
-  
-  export default Footer
+  return (
+    <div className="bottomFooter">
+      <footer>
+        <p>Created by C. Apostoli, J. Hanson & A. Hood - 2022</p>
+        <p>
+          Data provided by{" "}
+          <a href="https://coinranking.com/" target="_blank">
+            Coinranking
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
